74d636fc7a7fab1cca00ab2b5c3b7ab67396f3aa
SkilledSparrow <as850082@gmail.com>
2023-04-22 13:33:49 +0530

    Header + Footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import "./style.scss"
+import React from "react";
+import {
+    FaGithub,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
 
 const Footer = () => {
-  return (
-    <div>footer</div>
-  )
-}
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <a href="https://github.com/SkilledSparrow"><FaGithub /></a>
+                    </span>
+                    <span className="icon">
+                        <a href="https://www.instagram.com/skilled__sparrow/"><FaInstagram /></a>
+                    </span>
+                    <span className="icon">
+                        <a href=""><FaTwitter /></a>
+                    </span>
+                    <span className="icon">
+                        <a href="https://www.linkedin.com/in/abhishek-sharma-750a4a243/"><FaLinkedin /></a>
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
