b081c647b7df10534e4221cfeed15af62d06e2e5
SkilledSparrow <as850082@gmail.com>
2023-04-23 11:27:51 +0530

    Explore page added (99% done)

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,13 @@ import "./style.scss"
 import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
+import Cast from './cast/Cast'
+import VideosSection from './videosSection/VideosSection'
+import Similar from './carousels/similar'
+import Recommendation from './carousels/Recommendations'
+Similar
+Recommendation
+
 
 const Details = () => {
   const { mediaType, id} = useParams();
@@ -11,6 +18,10 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   )
 }
